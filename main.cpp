d65fccb4ac77f435e087108d982444cd6342e7b9
Zmrd13 <weders009@gmail.com>
2022-09-19 03:35:11 +0700

    random gen improved

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include "crypt.h"
 using namespace  std;
 int main() {
-
+    srand(time(0));
+    lli pid=rand() % (10000000000-2) + 2;;
+    cout<<pid;
+    while(check_prime(pid)!=1){
+        pid=rand() % (10000000000-2) + 2;;
+         cout<<"enter";
+    }
+dh(pid,0,0,2,0,6,0,0);
     return 0;
 }
